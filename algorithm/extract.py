7582285ca0f79a5664490879230728a8525fd777
Zuohan Zhao <zzhmark@126.com>
2020-10-17 02:11:12 +0800

    pipeline completed

--- algorithm/extract.py
+++ algorithm/extract.py
@@ -3,26 +3,46 @@ import cv2
 
 
 def std_filter(image, kernel):
-    a = image.astype(np.float)
-    return cv2.sqrt(cv2.blur(a ** 2, kernel) - cv2.blur(a, kernel) ** 2)
+    image = image.astype(np.float)
+    image = cv2.sqrt(
+        cv2.blur(image ** 2, kernel) -
+        cv2.blur(image, kernel) ** 2
+    )
+    return image
 
 
 def fill_hole(mask):
-    contour = cv2.findContours(mask, cv2.RETR_EXTERNAL,
-                               cv2.CHAIN_APPROX_NONE)[0]
-    return cv2.drawContours(mask, contour, -1, 255, cv2.FILLED)
+    contour = cv2.findContours(
+        mask, cv2.RETR_EXTERNAL,
+        cv2.CHAIN_APPROX_NONE
+    )[0]
+    mask = cv2.drawContours(
+        mask, contour, -1,
+        255, cv2.FILLED
+    )
+    return mask
 
 
-def extract(image, kernel=(3, 3), threshold=3, mask_only=False):
-    std = std_filter(cv2.cvtColor(image,
-                                  cv2.COLOR_BGR2GRAY),
-                     kernel)
-    thr = cv2.threshold(std, threshold, 255,
-                        cv2.THRESH_BINARY)[1].astype(np.uint8)
+def extract(image, filterSize=3,
+            threshold=3, mask_only=False):
+    assert type(image) is np.ndarray
+    assert type(filterSize) is int
+    kernel = (filterSize, filterSize)
+    std = std_filter(
+        image if len(image.shape) == 2 else \
+            cv2.cvtColor(image, cv2.COLOR_BGR2GRAY),
+        kernel
+    )
+    thr = cv2.threshold(
+        std, threshold, 255,
+        cv2.THRESH_BINARY
+    )[1].astype(np.uint8)
     mask = fill_hole(thr)
-    if mask_only:
-        image = None
-    else:
-        mask_bgr = cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
-        image = cv2.bitwise_or(image, cv2.bitwise_not(mask_bgr))
+    image = None if mask_only else \
+        cv2.bitwise_or(
+            image,
+            cv2.bitwise_not(
+                cv2.cvtColor(mask, cv2.COLOR_GRAY2BGR)
+            )
+        )
     return image, mask
